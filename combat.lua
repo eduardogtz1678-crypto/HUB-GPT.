10a66e36ac40978219bea2cd5d468d402737edb3
eduardogtz1678-crypto <eduardogtz1678@gmail.com>
2026-03-25 08:57:47 -0300

    Update combat.lua

--- combat.lua
+++ combat.lua
@@ -1,1 +1,23 @@
+spawn(function()
+    while task.wait() do
+        if _G.Settings.AutoFarm then
+            local p = game.Players.LocalPlayer
+            local c = p.Character
+            local root = c and c:FindFirstChild("HumanoidRootPart")
 
+            if root then
+                for _,v in pairs(workspace:GetChildren()) do
+                    local hum = v:FindFirstChild("Humanoid")
+                    local hrp = v:FindFirstChild("HumanoidRootPart")
+
+                    if hum and hrp and hum.Health > 0 then
+                        root.CFrame = hrp.CFrame * CFrame.new(0,3,5)
+
+                        game:GetService("VirtualInputManager"):SendMouseButtonEvent(0,0,0,true,game,0)
+                        game:GetService("VirtualInputManager"):SendMouseButtonEvent(0,0,0,false,game,0)
+                    end
+                end
+            end
+        end
+    end
+end)
