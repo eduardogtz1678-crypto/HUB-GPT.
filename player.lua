4eca734333a02b215fa918773d7ceeeca8e95f66
eduardogtz1678-crypto <eduardogtz1678@gmail.com>
2026-03-25 08:58:58 -0300

    Update player.lua

--- player.lua
+++ player.lua
@@ -1,1 +1,13 @@
+spawn(function()
+    while task.wait() do
+        if _G.Settings.AutoDodge then
+            local char = game.Players.LocalPlayer.Character
+            local hum = char and char:FindFirstChild("Humanoid")
+            local root = char and char:FindFirstChild("HumanoidRootPart")
 
+            if hum and root and hum.Health/hum.MaxHealth < 0.3 then
+                root.CFrame = root.CFrame * CFrame.new(0,0,20)
+            end
+        end
+    end
+end)
